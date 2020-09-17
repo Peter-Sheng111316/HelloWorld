6ddab0b6cfbba351f326b5b69d552643e742c0f3
Peter-Sheng111316 <shengp2023@york.org>
2020-09-16 22:31:33 -0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
     public static void main(String[] args) {
         //Use a comment like this to write your name!
-        //Like this: Ryo
+        //Like this: Ryo, Peter
     }
 }
